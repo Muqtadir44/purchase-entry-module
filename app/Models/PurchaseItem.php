99f5e4ed36a12acd6e85ebfeaa8caf5668adc8d7
Muqtadir44 <kalhoromuqtadir125@gmail.com>
2026-06-11 16:26:09 +0500

    guarded

--- app/Models/PurchaseItem.php
+++ app/Models/PurchaseItem.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class PurchaseItem extends Model
 {
+    protected $guarded = [];
     public function purchase()
     {
         return $this->belongsTo(Purchase::class);
